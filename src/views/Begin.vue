e2fb0063fbe491883894759aa8576f939329a2c9
Lucas Emanuel <lucaxemanuel@gmail.com>
2020-05-12 22:39:06 -0300

    Adicionando visão para segregar componentes

---
+++ src/views/Begin.vue
@@ -0,0 +1,49 @@
+<template>
+  <Book :title="title"
+    :store="store"
+    :reverse="reverse"
+    :photo="photo"
+    :stylePhoto="stylePhoto"
+  />
+</template>
+
+<script>
+import Book from '../components/Book.vue';
+
+export default {
+  name: 'Begin',
+  components: {
+    Book
+  },
+  props: ['reverse'],
+  data() {
+    const data = {
+      title: "Inicio",
+      store: store,
+      reverse: false
+    }
+    return Object.assign({}, data, this.todo);
+  }
+}
+
+const store = `
+  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+  Tempora modi repellendus necessitatibus maiores. Quod voluptatibus lab
+  orum adipisci nobis sint magnam officia odit facilis expedita qui, magni min
+  ima iusto est ut!Lorem ipsum dolor sit amet consectetur adipisicing elit.
+  Tempora modi repellendus necessitatibus maiores. Quod voluptatibus lab
+  orum adipisci nobis sint magnam officia odit facilis expedita qui, magni min
+  ima iusto est ut!
+`;
+
+</script>
+
+<style>
+  div.img {
+    background: linear-gradient(rgba(255, 255, 255, 0), rgba(0, 0, 0, .75)), url("https://live.staticflickr.com/65535/49882402503_e1306a2076_b.jpg");
+    /* background: #c4c4c4; */
+    background-repeat: no-repeat;
+    background-position: 50%;
+    background-size: cover;
+  }
+</style>
